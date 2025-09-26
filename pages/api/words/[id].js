dd1574a41eb9aaa24ad358ce97ba29a9b1a94bf4
Md. Toha <m.i.toaha14@gmail.com>
2025-09-26 20:14:59 +0600

    Cross origin problems

--- pages/api/words/[id].js
+++ pages/api/words/[id].js
@@ -9,8 +9,8 @@ export default async function handler(req, res) {
   // Inside your serverless function handler
   const allowedOrigins = [
     "http://localhost:5173",                       // local dev
-    "https://word-learning-lyart.vercel.app",     // Vercel frontend
-    "https://shimmering-manatee-e3a37f.netlify.app/" // Netlify frontend
+    "https://chipper-moonbeam-9ae359.netlify.app/",     // Vercel frontend
+    "https://shimmering-manatee-e3a37f.netlify.app", // Netlify frontend
   ];
 
   const origin = req.headers.origin;
